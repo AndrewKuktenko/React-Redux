c03ab851e3ff7dc810535048e1f7e55632b9f76d
AndrewKuktenko <andriy.kuktenko@gmail.com>
2018-02-08 01:49:30 +0200

    live reload, redux added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import 'babel-polyfill'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-import {AppContainer} from 'react-hot-loader'
 
-ReactDOM.render(<AppContainer><App /></AppContainer>, document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import configureStore from './store/configureStore'
+import { AppContainer } from 'react-hot-loader';
+
+const store = configureStore()
+
+const render = (Component) => {
+    ReactDOM.render(
+        <AppContainer>
+            <Provider store={store}>
+                <Component />
+            </Provider>
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};
+
+render(App);
 
 if (module.hot) {
-    module.hot.accept(() => {
-        ReactDOM.render(<AppContainer><App /></AppContainer>, document.getElementById('root'));
+    module.hot.accept('./containers/App', () => {
+        const App = require('./containers/App').default;
+        render(App)
     })
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+    name: 'Andrew',
+    surname: 'Kuktenko',
+    age: 21
+};
+
+export default function userstate(state = initialState) {
+    return state;
+}
